b7f6382c432b49d6a1538e27407e9d6e31750b46
Andrés <andresconsani03@gmail.com>
2024-07-31 08:22:36 -0400

    Prueba 1: Agregando dependencias

--- src/models/Usuarios.js
+++ src/models/Usuarios.js
@@ -1,77 +1,79 @@
-import sqlite from 'better-sqlite3';
+// import sqlite from 'better-sqlite3';
 
-const db = new sqlite('../server/database.db');
+// const db = new sqlite('../server/database.db');
 
-db.prepare(
-    'CREATE TABLE IF NOT EXISTS Usuarios' +
-    '(id INTEGER PRIMARY KEY AUTOINCREMENT, usuario TEXT UNIQUE, pass TEXT, type INTEGER)').run();
+// db.prepare(
+//     'CREATE TABLE IF NOT EXISTS Usuarios' +
+//     '(id INTEGER PRIMARY KEY AUTOINCREMENT, usuario TEXT UNIQUE, pass TEXT, type INTEGER)').run();
 
-function comprobarAdmin(adminID) {
-    let json = db.prepare(`SELECT * FROM Usuarios WHERE id = ? AND type = 1`).get(adminID);
-    return (json != null);
-}
+// function comprobarAdmin(adminID) {
+//     let json = db.prepare(`SELECT * FROM Usuarios WHERE id = ? AND type = 1`).get(adminID);
+//     return (json != null);
+// }
 
-export default class $$Usuarios {
+// export default class $$Usuarios {
 
-    static create(adminID, user, pass, type) {
-        console.log(user.trim().length, pass.trim().length)
-        if (!comprobarAdmin(adminID)) throw new Error('No tienes permisos.');
-        if (user.trim().length < 3) throw new Error('El nombre de usuario debe tener como mínimo 3 caracteres.');
-        if (pass.trim().length < 6) throw new Error('La contraseña debe tener como mínimo 6 caracteres.');
+//     static create(adminID, user, pass, type) {
+//         console.log(user.trim().length, pass.trim().length)
+//         if (!comprobarAdmin(adminID)) throw new Error('No tienes permisos.');
+//         if (user.trim().length < 3) throw new Error('El nombre de usuario debe tener como mínimo 3 caracteres.');
+//         if (pass.trim().length < 6) throw new Error('La contraseña debe tener como mínimo 6 caracteres.');
 
-        try{
-            db.prepare(`INSERT INTO Usuarios (usuario, pass, type) VALUES(?, ?, ?)`).run(user.trim(), pass, type);
-        } catch (err){
-            let cadena = err.message.split(' ');
-            console.log(cadena);
-            if(cadena[0] == 'UNIQUE') throw new Error('Este usuario ya existe.');
-        }
+//         try{
+//             db.prepare(`INSERT INTO Usuarios (usuario, pass, type) VALUES(?, ?, ?)`).run(user.trim(), pass, type);
+//         } catch (err){
+//             let cadena = err.message.split(' ');
+//             console.log(cadena);
+//             if(cadena[0] == 'UNIQUE') throw new Error('Este usuario ya existe.');
+//         }
         
 
-        return { 'message': 'OK' };
-    }
+//         return { 'message': 'OK' };
+//     }
 
-    static read(adminID, user) {
-        let res
-        if (!comprobarAdmin(adminID)) throw new Error('No tienes permisos.');
-        if(user == -1){
-            res = db.prepare(`SELECT * FROM Usuarios`).all();
-        } else {
-            res = db.prepare(`SELECT * FROM Usuarios WHERE usuario LIKE ?`).all(`%${user}%`);
-        }
+//     static read(adminID, user) {
+//         let res
+//         if (!comprobarAdmin(adminID)) throw new Error('No tienes permisos.');
+//         if(user == -1){
+//             res = db.prepare(`SELECT * FROM Usuarios`).all();
+//         } else {
+//             res = db.prepare(`SELECT * FROM Usuarios WHERE usuario LIKE ?`).all(`%${user}%`);
+//         }
         
-        return res;
-    }
+//         return res;
+//     }
 
-    static update(adminID, userID, newU, newP, newT) {
-        if (!comprobarAdmin(adminID)) throw new Error('No tienes permisos.');
-        if (newU.length < 3) throw new Error('El nombre de usuario debe tener como mínimo 3 caracteres.');
-        if (newP.length < 6) throw new Error('La contraseña debe tener como mínimo 6 caracteres.');
-        let res = db.prepare(`UPDATE Usuarios SET usuario = ?, pass = ?, type = ? WHERE id = ?`).run(newU, newP, newT, userID);
-        return res;
+//     static update(adminID, userID, newU, newP, newT) {
+//         if (!comprobarAdmin(adminID)) throw new Error('No tienes permisos.');
+//         if (newU.length < 3) throw new Error('El nombre de usuario debe tener como mínimo 3 caracteres.');
+//         if (newP.length < 6) throw new Error('La contraseña debe tener como mínimo 6 caracteres.');
+//         let res = db.prepare(`UPDATE Usuarios SET usuario = ?, pass = ?, type = ? WHERE id = ?`).run(newU, newP, newT, userID);
+//         return res;
 
-    }
+//     }
 
-    static delete(adminID, userID) {
-        if (!comprobarAdmin(adminID)) throw new Error('No tienes permisos.');
-        db.prepare(`DELETE FROM Usuarios WHERE id = ?`).run(userID);
-        return { 'message': 'OK' };
+//     static delete(adminID, userID) {
+//         if (!comprobarAdmin(adminID)) throw new Error('No tienes permisos.');
+//         db.prepare(`DELETE FROM Usuarios WHERE id = ?`).run(userID);
+//         return { 'message': 'OK' };
 
-    }
+//     }
 
-    static login(user, pass) {
-        let json = db.prepare(`SELECT * FROM Usuarios WHERE usuario = ?`).get(user);
-        if (json) {
+//     static login(user, pass) {
+//         let json = db.prepare(`SELECT * FROM Usuarios WHERE usuario = ?`).get(user);
+//         if (json) {
 
-            if (json.pass == pass) {
-                return { 'message': 'OK' };
-            } else {
-                throw new Error('El usuario o contraseña son incorrectos.');
-            }
+//             if (json.pass == pass) {
+//                 return { 'message': 'OK' };
+//             } else {
+//                 throw new Error('El usuario o contraseña son incorrectos.');
+//             }
 
-        } else {
-            throw new Error('Usuario o contraseñas inválidos.');
-        }
-    }
+//         } else {
+//             throw new Error('Usuario o contraseñas inválidos.');
+//         }
+//     }
 
-}
+// }
+
+console.log('hola');
