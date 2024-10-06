860f47d59ce186e28ccd6b914f38c8fd374de1b3
Andrés <andresconsani03@gmail.com>
2024-10-06 15:42:16 -0400

    Bug: token validation

--- src/validaciones.js
+++ src/validaciones.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const SECRET_KEY = 'holacomoestasyomuybienytuahconchaleyobien38959193123812938194810948198349120';
 
 export function authenticateToken(req, res, next) {
-    const token = req.cookies.token;
+    const token = req.cookies.user;
 
     if (!token) return res.sendStatus(401);
 
